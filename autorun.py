a2088f3023103b332165e63375ab268944e75401
Potato-Cracker <maheswarbora2@gmail.com>
2024-03-28 11:20:55 +0530

    directory correction

--- autorun.py
+++ autorun.py
@@ -2,7 +2,7 @@ import os
 from shutil import copy
 from subprocess import run
 
-configs_dir = '../Configs'
+configs_dir = './Configs'
 
 for config_file in os.listdir(configs_dir):
     copy(os.path.join(configs_dir,config_file),"./config.yaml")
